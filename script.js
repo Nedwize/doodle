b4f463248baf100a4ee3126980197cc44f55f82d
NakshatraCodes <nakshatra163@gmail.com>
2021-01-11 22:08:09 +0530

    Add Customizations to Canvas

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 window.addEventListener("load", () => {
+
     const canvas = document.querySelector("#myCanvas");
     const cntx = canvas.getContext("2d");
 
+    
+    let thicknessInput = document.getElementById("thickness");
+    let colorInput = document.getElementById("color");
+
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    cntx.lineWidth = 5;
 
     let painting = false;
 
     function startPosition(e){
+        console.log("Mouse Down");
         painting = true;
         draw(e);
     }
 
-    function finishedPosition() {
+    function finishedPosition(){
+        console.log("Mouse Up");
         painting = false;
         cntx.beginPath();
     }
 
     function draw(e) {
         if (!painting) return;
-        cntx.lineWidth = 5;
         cntx.lineCap = "round";
         cntx.lineJoin = 'round';
-
+        console.log('Drawing');
         cntx.lineTo(e.clientX, e.clientY);
         cntx.stroke();
         cntx.beginPath();
         cntx.moveTo(e.clientX, e.clientY);
     }
+
+    function changeThickness() {
+        thickness = document.getElementById("thickness").value;
+        console.log(thickness)
+        cntx.lineWidth = thickness;
+    }
+
+    function changeColor() {
+        color = document.getElementById("color").value;
+        cntx.strokeStyle = color;
+    }
     // Events
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
     canvas.addEventListener("mousemove", draw);
+    thicknessInput.addEventListener("change", changeThickness);
+    colorInput.addEventListener("change", changeColor);
 })
