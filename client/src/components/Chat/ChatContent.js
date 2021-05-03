c6b2cd375d1a80a6748af2c9c5e0c771c2a846bb
NakshatraCodes <nakshatra163@gmail.com>
2021-05-04 01:57:13 +0530

    Refactor ChatBox Components

---
+++ client/src/components/Chat/ChatContent.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.p`
+  padding-left: 8px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  margin: 0;
+`;
--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -2,9 +2,22 @@ import ChatBox from "./ChatBox";
 import ChatMessage from "./ChatMessage";
 import ChatContainer from "./ChatContainer";
 import InputBox from "./InputBox";
+import ChatContent from "./ChatContent";
 
 const arr = [
-  "17689900968790099879688767",
+  "1768990097",
+  "aasfusgaoasin",
+  "afhausdhas",
+  "aishosahd",
+  "paishfoa",
+  "aisfhalsh",
+  "1768990097",
+  "aasfusgaoasin",
+  "afhausdhas",
+  "aishosahd",
+  "paishfoa",
+  "aisfhalsh",
+  "1768990097",
   "aasfusgaoasin",
   "afhausdhas",
   "aishosahd",
@@ -15,19 +28,12 @@ const arr = [
 const Chat = () => {
   return (
     <ChatBox>
-      <InputBox type="text" placeholder="Type Your Guess Here" style={{}} />
+      <InputBox type="text" placeholder="Type Your Guess Here" />
       <ChatContainer>
         {arr.map((element) => {
           return (
             <ChatMessage>
-              <p
-                style={{
-                  paddingLeft: "8px",
-                  paddingTop: "4px",
-                  paddingBottom: "4px",
-                  margin: "0",
-                }}
-              >{`Aman : ${element}`}</p>
+              <ChatContent>{`Aman : ${element}`}</ChatContent>
             </ChatMessage>
           );
         })}
